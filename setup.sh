b64e78b0c61f56dff33775ae3471beadcec0645c
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2025-04-16 02:26:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install torch torchvision torchaudio
+
 mkdir -p models/bert models/gpt2 models/roberta models/fusion data/raw data/processed data/embeddings
 echo "✅ Setup complete. You can now run the app using:" echo " streamlit run app/streamlit_app.py"
