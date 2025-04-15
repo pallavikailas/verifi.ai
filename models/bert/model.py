717222e7c9f5d281071e5698acdd129d1b28c623
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2025-04-15 17:57:58 +0530

    Update model.py

--- models/bert/model.py
+++ models/bert/model.py
@@ -1,18 +1,13 @@
-from transformers import BertModel
-import torch.nn as nn
-
 class BERTClassifier(nn.Module):
     def __init__(self):
-        super(BERTClassifier, self).__init__()
+        super().__init__()
         self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.classifier = nn.Sequential(
-            nn.Linear(768, 256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(256, 2)
-        )
+            nn.Linear(768, 256), 
+            nn.ReLU(), 
+            nn.Dropout(0.3), 
+            nn.Linear(256, 2))
 
     def forward(self, input_ids, attention_mask):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        cls_output = outputs.pooler_output
-        return self.classifier(cls_output)
+        output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        return self.classifier(output.pooler_output)
