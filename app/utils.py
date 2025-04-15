78b96102da973d0ab3ee4eaf0bc429b5d441166c
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2025-04-16 02:40:06 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,13 @@
+import sys
+import os
+
+# Add parent directory to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from scripts.inference_pipeline import predict_news
+
+def run_inference_pipeline(text):
+    return predict_news(text)
 
 def run_inference_pipeline(text):
     return predict_news(text)
